0318ddc85ff559e257c6a3014ba3c7129d53c3fb
snaker938 <jmendel123@gmail.com>
2022-02-02 16:30:19 +0000

    changed code

--- 83.js
+++ 83.js
@@ -14,52 +14,36 @@ let btn_upload = document
 
 function next(results) {
   let results2 = Array(results)[0].data;
+  results2.pop();
   dijkstra(results2, [0, 0], [79, 79]);
 }
 
-//   function generateTableHead(table, data) {
-//     let thead = table.createTHead();
-//     let row = thead.insertRow();
-//     for (let key of data) {
-//       let th = document.createElement('th');
-//       let text = document.createTextNode(key);
-//       th.appendChild(text);
-//       row.appendChild(th);
+function dijkstra(grid, startCoords, endCoords) {}
+
+// function dijkstra(grid, startIndex, endIndex) {
+//   // start();
+//   console.log(grid);
+//   function start() {
+//     let grid2 = cloneVariable(grid);
+//     let rowNum = grid[0].length;
+//     let colNum = grid.length - 1;
+
+//     for (let row = 0; row < rowNum; row++) {
+//       for (let col = 0; col < colNum; col++) {
+//         if (row == startIndex[0] && col == startIndex[1]) {
+//           grid2[startIndex[0]][startIndex[1]] =
+//             grid[startIndex[0]][startIndex[1]];
+//         } else {
+//           grid2[row][col] = Infinity;
+//         }
+//       }
 //     }
+//     grid2.pop();
 //   }
 
-//   function generateTableRows(table, data) {
-//     let newRow = table.insertRow(-1);
-//     data.map((row, index) => {
-//       let newCell = newRow.insertCell();
-//       let newText = document.createTextNode(row);
-//       newCell.appendChild(newText);
-//     });
-//   }
-
-function dijkstra(grid, startIndex, endIndex) {
-  start();
-  function start() {
-    let grid2 = cloneVariable(grid);
-    let rowNum = grid[0].length;
-    let colNum = grid.length - 1;
-
-    for (let row = 0; row < rowNum; row++) {
-      for (let col = 0; col < colNum; col++) {
-        if (row == startIndex[0] && col == startIndex[1]) {
-          grid2[startIndex[0]][startIndex[1]] =
-            grid[startIndex[0]][startIndex[1]];
-        } else {
-          grid2[row][col] = Infinity;
-        }
-      }
-    }
-    grid2.pop();
-  }
-
-  let frontier = [startIndex];
-  while (frontier.length > 0) {}
-}
+//   let frontier = [startIndex];
+//   while (frontier.length > 0) {}
+// }
 
 function cloneVariable(variableData) {
   return JSON.parse(JSON.stringify(variableData));
